0545e0e34f443013550f6baf74bb969d7a4fd83a
Lilacs <zp1990@me.com>
2014-10-18 00:06:53 +0800

    finish the work

--- Fortuna/AppDelegate.swift
+++ Fortuna/AppDelegate.swift
@@ -11,13 +11,36 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
-
-
+    var window: UIWindow!
+    var positiveQuotes: [String]!
+    var negativeQuotes: [String]!
+    let positiveQuotesPath = NSBundle.mainBundle().pathForResource("positiveQuotes", ofType: "json")
+    let negativeQuotesPath = NSBundle.mainBundle().pathForResource("negativeQuotes", ofType: "json")
+    
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        if(positiveQuotesPath == nil || negativeQuotesPath == nil) {
+            positiveQuotes = ["Failed to read data from"]
+            negativeQuotes = ["Failed to read data from"]
+        }else {
+            positiveQuotes = loadQuote(positiveQuotesPath!)
+            negativeQuotes = loadQuote(negativeQuotesPath!)
+        }
+        
         return true
     }
+    
+    
+    func loadQuote(fileName: String) -> [String]? {
+        var random = Int(arc4random_uniform(100))
+        let data = NSData(contentsOfFile: fileName)
+        
+        var err : NSError?
+        let json = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.allZeros, error: &err) as [String]
+        assert(err == nil, "Error parsing json: \(err)")
+        
+        return json
+    }
+
 
     func applicationWillResignActive(application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
--- Fortuna/ViewController.swift
+++ Fortuna/ViewController.swift
@@ -9,17 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    let path = NSBundle.mainBundle().pathForResource("positiveQuotes", ofType: "json")
+    
+    @IBOutlet weak var quotationTextView: UITextView!
+    var delegate: AppDelegate!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        delegate = UIApplication.sharedApplication().delegate as AppDelegate
+        displayRandomQuote()
     }
-
+    
+    
+    @IBAction func rootViewTapped(sender: UITapGestureRecognizer) {
+        displayRandomQuote()
+    }
+    
+    func displayRandomQuote() {
+        let usePositive = Int(arc4random_uniform(2)) == 0
+        if(usePositive){
+            displayRandomQuoteFrom(delegate.positiveQuotes)
+        }else{
+            displayRandomQuoteFrom(delegate.negativeQuotes)
+        }
+    }
+    
+    func displayRandomQuoteFrom(quote: [String]) {
+        var random = Int(arc4random_uniform(UInt32(quote.count)))
+        quotationTextView.text = quote[random]
+    }
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
-
-
+    
+    
 }
 
